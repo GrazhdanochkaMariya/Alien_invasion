572b0fe99d709abc6bbd66e424ad6b84da5ac3b1
Mariya <mariyashakuro@gmail.com>
2022-02-16 20:39:06 +0300

    created several rows of alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,17 +76,29 @@ class AlienInvasion:
         # Creates alien and counts number of aliens in raw.
         # The interval between aliens is equal to the width of the alien.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Creates first raw of aliens.
-        for alien_number in range(number_aliens_x):
-            # Creates alien and  place it in raw.
-            alien = Alien(self)
-            alien_x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien_x
-            self.aliens.add(alien)
+        # Defines number of rows fit on the screen.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Creates fleet.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Creates alien and  place it in row."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Updates the images on the screen and displays a new screen."""
